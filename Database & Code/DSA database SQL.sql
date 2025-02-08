8d6f53a179c62e0e385a5e1a4ec5ca9e89cde9fc
Mikula0411 <128592139+Mikula0411@users.noreply.github.com>
2025-02-08 03:40:00 +0000

    Database SQL
    didnt know any of the place of interest in which borough, can you guy give me the borough of each POI

---
+++ Database & Code/DSA database SQL.sql
@@ -0,0 +1,116 @@
+-- CREATE TABLE City (
+--     City_ID INT PRIMARY KEY AUTO_INCREMENT,
+--     Name VARCHAR(255) NOT NULL,
+--     Country VARCHAR(100) NOT NULL,
+--     Population INT,
+--     Size FLOAT,
+--     Latitude DECIMAL(9,6),
+--     Longitude DECIMAL(9,6),
+--     Timezone VARCHAR(50),
+--     Language VARCHAR(50),
+--     Elevation FLOAT
+-- );
+
+-- CREATE TABLE Type (
+--     Type_ID INT PRIMARY KEY AUTO_INCREMENT,
+--     Name VARCHAR(20) NOT NULL
+-- );
+
+-- CREATE TABLE Place_Of_Interest (
+--     Place_ID INT PRIMARY KEY AUTO_INCREMENT,
+--     City_ID INT NOT NULL,
+--     Type_ID INT NOT NULL,
+--     Name VARCHAR(255) NOT NULL,
+--     Address VARCHAR(255),
+--     Description TEXT,
+--     Photos VARCHAR(255), -- Stores URL or file path
+--     Opening_Time TIME,
+--     Ending_Time TIME,
+--     FOREIGN KEY (City_ID) REFERENCES City(City_ID),
+--     FOREIGN KEY (Type_ID) REFERENCES Type(Type_ID)
+-- );
+
+-- CREATE TABLE Borough (
+--     Borough_ID INT AUTO_INCREMENT PRIMARY KEY,
+--     City_ID INT,
+--     Place_ID INT,
+--     Name VARCHAR(20),
+--     FOREIGN KEY (City_ID) REFERENCES City(City_ID),
+--     FOREIGN KEY (Place_ID) REFERENCES Place_of_Interest(Place_ID)
+-- );
+
+-- INSERT INTO City (Name, Country, Population, Size, Latitude, Longitude, timezone, language, elevation)
+-- VALUES 
+-- ('London', 'United Kingdom', 8982000, 1572.0, 51.507351, -0.127758, 'GMT+0', 'English', 11.0),
+-- ('New York', 'United States', 8419600, 783.8, 40.712776, -74.005974, 'GMT-5', 'English', 10.0);
+
+-- INSERT INTO Type (Name)
+-- VALUES
+-- ('Restaurants'),
+-- ('Train Stations'),
+-- ('Universities'),
+-- ('Banks'),
+-- ('Museums'),
+-- ('5 Star Hotels');
+
+-- INSERT INTO Place_of_Interest (City_ID, Type_ID, Name, Address, Description, Photos, Opening_time, Ending_time)
+-- VALUES
+-- (1, 1, 'Scully', '4 St James''s Market, London SW1Y 4AH', 'Restaurant', 'https://maps.app.goo.gl/EUoWdWPmaVrzdMW86', '08:00:00', '22:00:00'),
+-- (1, 2, 'Charing Cross', 'Strand, London WC2N 5HF', 'Train Station', 'https://maps.app.goo.gl/cbfazXPBAonrairT7', '05:00:00', '23:00:00'),
+-- (1, 3, 'University of Notre Dame (USA) in England', '1 Suffolk St, London SW1Y 4HG', 'University', 'https://maps.app.goo.gl/WtTH4NThc6PMDUz37', '09:00:00', '18:00:00'),
+-- (1, 4, 'HSBC Covent Garden', '16 King St, London WC2E 8JF', 'Bank', 'https://maps.app.goo.gl/UUyuVT8X8VyjKJtU8', '09:00:00', '17:00:00'),
+-- (1, 5, 'The National Gallery', 'Trafalgar Square, London WC2N 5DN', 'Museum', 'https://maps.app.goo.gl/F9eyK7PWGuDQHkUF6', '10:00:00', '18:00:00'),
+-- (1, 6, 'Corinthia London', 'Whitehall Pl, London SW1A 2BD', '5 Star Hotel', 'https://maps.app.goo.gl/fX6BjTLCkWvhs67c6', '00:00:00', '23:59:59');
+
+-- INSERT INTO Place_of_Interest (City_ID, Type_ID, Name, Address, Description, Photos, Opening_time, Ending_time)
+-- VALUES
+-- (2, 1, 'Reserve Cut', '40 Broad St 2nd Floor, New York, NY 10004, United States', 'Restaurant', 'https://maps.app.goo.gl/ezKq5Ypfi1JZFwza9', '08:00:00', '22:00:00'),
+-- (2, 2, 'World Trade Center', '10007, 70 Vesey St, New York, 10006, United States', 'Train Station', 'https://maps.app.goo.gl/vxgtg49XBF42KniS8', '05:00:00', '23:00:00'),
+-- (2, 3, 'New York University', 'New York, NY 10012, United States', 'University', 'https://maps.app.goo.gl/hhitytMNzU5awz3m9', '09:00:00', '18:00:00'),
+-- (2, 4, 'Bank of America Financial Center', '50 Bayard St, New York, NY 10013, United States', 'Bank', 'https://maps.app.goo.gl/bi3799mW18z4xZMo8', '09:00:00', '17:00:00'),
+-- (2, 5, '9/11 Memorial & Museum', '180 Greenwich St, New York, NY 10007, United States', 'Museum', 'https://maps.app.goo.gl/bqe8yoCUKxfzaZRHA', '10:00:00', '18:00:00'),
+-- (2, 6, 'The Beekman, A Thompson Hotel, by Hyatt', '123 Nassau St, New York, NY 10038, United States', '5 Star Hotel', 'https://maps.app.goo.gl/ecbsWgFcbiMLDstj9', '00:00:00', '23:59:59');
+
+-- INSERT INTO Borough (City_ID, Name)
+-- VALUES
+-- (1, 'Barking and Dagenham'),
+-- (1, 'Barnet'),
+-- (1, 'Bexley'),
+-- (1, 'Brent'),
+-- (1, 'Bromley'),
+-- (1, 'Camden'),
+-- (1, 'Croydon'),
+-- (1, 'Ealing'),
+-- (1, 'Enfield'),
+-- (1, 'Greenwich'),
+-- (1, 'Hackney'),
+-- (1, 'Hammersmith and Fulham'),
+-- (1, 'Haringey'),
+-- (1, 'Harrow'),
+-- (1, 'Havering'),
+-- (1, 'Hillingdon'),
+-- (1, 'Hounslow'),
+-- (1, 'Islington'),
+-- (1, 'Kensington and Chelsea'),
+-- (1, 'Kingston upon Thames'),
+-- (1, 'Lambeth'),
+-- (1, 'Lewisham'),
+-- (1, 'Merton'),
+-- (1, 'Newham'),
+-- (1, 'Redbridge'),
+-- (1, 'Richmond upon Thames'),
+-- (1, 'Southwark'),
+-- (1, 'Sutton'),
+-- (1, 'Tower Hamlets'),
+-- (1, 'Waltham Forest'),
+-- (1, 'Wandsworth'),
+-- (1, 'Westminster'),
+-- (1, 'City of London');
+
+-- INSERT INTO Borough (City_ID, Name)
+-- VALUES
+-- (2, 'Manhattan'),
+-- (2, 'Brooklyn'),
+-- (2, 'Queens'),
+-- (2, 'The Bronx'),
+-- (2, 'Staten Island');

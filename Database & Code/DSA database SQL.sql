81c1d2283bdc1b6071afdea7fc334eaac5edc8a3
Mikula0411 <128592139+Mikula0411@users.noreply.github.com>
2025-02-14 04:17:22 +0000

    Update DSA database SQL.sql

--- Database & Code/DSA database SQL.sql
+++ Database & Code/DSA database SQL.sql
@@ -1,4 +1,6 @@
--- CREATE TABLE City (
+-- CREATE DATABASE IF NOT EXISTS twin_city;
+
+-- CREATE TABLE IF NOT EXISTS City (
 --     City_ID INT PRIMARY KEY AUTO_INCREMENT,
 --     Name VARCHAR(255) NOT NULL,
 --     Country VARCHAR(100) NOT NULL,
@@ -11,65 +13,61 @@
 --     Elevation FLOAT
 -- );
 
--- CREATE TABLE Type (
+-- CREATE TABLE IF NOT EXISTS Type (
 --     Type_ID INT PRIMARY KEY AUTO_INCREMENT,
 --     Name VARCHAR(20) NOT NULL
 -- );
 
--- CREATE TABLE Place_Of_Interest (
+-- CREATE TABLE IF NOT EXISTS Place_Of_Interest (
 --     Place_ID INT PRIMARY KEY AUTO_INCREMENT,
 --     City_ID INT NOT NULL,
 --     Type_ID INT NOT NULL,
 --     Name VARCHAR(255) NOT NULL,
 --     Address VARCHAR(255),
+--     Latitude DECIMAL(18,15),
+--     Longitude DECIMAL(18,15),
 --     Description TEXT,
---     Photos VARCHAR(255), -- Stores URL or file path
+--     Photos VARCHAR(255),
 --     Opening_Time TIME,
 --     Ending_Time TIME,
---     FOREIGN KEY (City_ID) REFERENCES City(City_ID),
---     FOREIGN KEY (Type_ID) REFERENCES Type(Type_ID)
+--     FOREIGN KEY (City_ID) REFERENCES City(City_ID) ON DELETE CASCADE,
+--     FOREIGN KEY (Type_ID) REFERENCES Type(Type_ID) ON DELETE CASCADE
 -- );
 
--- CREATE TABLE Borough (
+-- CREATE TABLE IF NOT EXISTS Borough (
 --     Borough_ID INT AUTO_INCREMENT PRIMARY KEY,
 --     City_ID INT,
---     Place_ID INT,
---     Name VARCHAR(20),
---     FOREIGN KEY (City_ID) REFERENCES City(City_ID),
---     FOREIGN KEY (Place_ID) REFERENCES Place_of_Interest(Place_ID)
+--     Place_ID INT DEFAULT NULL,
+--     Name VARCHAR(100) NOT NULL,
+--     FOREIGN KEY (City_ID) REFERENCES City(City_ID) ON DELETE CASCADE,
+--     FOREIGN KEY (Place_ID) REFERENCES Place_Of_Interest(Place_ID) ON DELETE CASCADE
 -- );
 
--- INSERT INTO City (Name, Country, Population, Size, Latitude, Longitude, timezone, language, elevation)
--- VALUES 
--- ('London', 'United Kingdom', 8982000, 1572.0, 51.507351, -0.127758, 'GMT+0', 'English', 11.0),
--- ('New York', 'United States', 8419600, 783.8, 40.712776, -74.005974, 'GMT-5', 'English', 10.0);
+-- INSERT INTO City (Name, Country, Population, Size, Latitude, Longitude, timezone, language, elevation) VALUES 
+--     ('London', 'United Kingdom', 8982000, 1572.0, 51.507351, -0.127758, 'GMT+0', 'English', 11.0),
+--     ('New York', 'United States', 8419600, 783.8, 40.712776, -74.005974, 'GMT-5', 'English', 10.0);
 
 -- INSERT INTO Type (Name)
--- VALUES
--- ('Restaurants'),
--- ('Train Stations'),
--- ('Universities'),
--- ('Banks'),
--- ('Museums'),
--- ('5 Star Hotels');
+--      VALUES ('Restaurants'), ('Train Stations'), ('Universities'), ('Banks'), ('Museums'), ('5 Star Hotels');
 
--- INSERT INTO Place_of_Interest (City_ID, Type_ID, Name, Address, Description, Photos, Opening_time, Ending_time)
--- VALUES
--- (1, 1, 'Scully', '4 St James''s Market, London SW1Y 4AH', 'Restaurant', 'https://maps.app.goo.gl/EUoWdWPmaVrzdMW86', '08:00:00', '22:00:00'),
--- (1, 2, 'Charing Cross', 'Strand, London WC2N 5HF', 'Train Station', 'https://maps.app.goo.gl/cbfazXPBAonrairT7', '05:00:00', '23:00:00'),
--- (1, 3, 'University of Notre Dame (USA) in England', '1 Suffolk St, London SW1Y 4HG', 'University', 'https://maps.app.goo.gl/WtTH4NThc6PMDUz37', '09:00:00', '18:00:00'),
--- (1, 4, 'HSBC Covent Garden', '16 King St, London WC2E 8JF', 'Bank', 'https://maps.app.goo.gl/UUyuVT8X8VyjKJtU8', '09:00:00', '17:00:00'),
--- (1, 5, 'The National Gallery', 'Trafalgar Square, London WC2N 5DN', 'Museum', 'https://maps.app.goo.gl/F9eyK7PWGuDQHkUF6', '10:00:00', '18:00:00'),
--- (1, 6, 'Corinthia London', 'Whitehall Pl, London SW1A 2BD', '5 Star Hotel', 'https://maps.app.goo.gl/fX6BjTLCkWvhs67c6', '00:00:00', '23:59:59');
+-- INSERT INTO Place_of_Interest (City_ID, Type_ID, Name, Address, Latitude, Longitude, Description, Photos, Opening_time, Ending_time)
+--     VALUES
+--     (1, 1, 'Scully', '4 St James''s Market, London SW1Y 4AH', 51.509136687911685, -0.1329663068511105,'Restaurant', 'https://i.imgur.com/zAp50yG.jpg', '08:00:00', '22:00:00'),
+--     (1, 2, 'Charing Cross', 'Strand, London WC2N 5HF', 51.50829422485241, -0.12483750236822497, 'Train Station', 'https://i.imgur.com/XSB8EMh.jpg', '05:00:00', '23:00:00'),
+--     (1, 3, 'University of Notre Dame (USA) in England', '1 Suffolk St, London SW1Y 4HG', 51.508420513914544, -0.1303707027499567, 'University', 'https://i.imgur.com/XLMl5aX.jpg', '09:00:00', '18:00:00'),
+--     (1, 4, 'HSBC Covent Garden', '16 King St, London WC2E 8JF', 51.51141719809663, -0.12495604692421396, 'Bank', 'https://i.imgur.com/nzcj5JW.jpg', '09:00:00', '17:00:00'),
+--     (1, 5, 'The National Gallery', 'Trafalgar Square, London WC2N 5DN', 51.50906252259639, -0.12836337575917522, 'Museum', 'https://i.imgur.com/IUKwxWf.jpg', '10:00:00', '18:00:00'),
+--     (1, 6, 'Corinthia London', 'Whitehall Pl, London SW1A 2BD', 51.50658047404813, -0.12433406041980899, '5 Star Hotel', 'https://i.imgur.com/4LCPpgy.jpg', '00:00:00', '23:59:59');
 
--- INSERT INTO Place_of_Interest (City_ID, Type_ID, Name, Address, Description, Photos, Opening_time, Ending_time)
--- VALUES
--- (2, 1, 'Reserve Cut', '40 Broad St 2nd Floor, New York, NY 10004, United States', 'Restaurant', 'https://maps.app.goo.gl/ezKq5Ypfi1JZFwza9', '08:00:00', '22:00:00'),
--- (2, 2, 'World Trade Center', '10007, 70 Vesey St, New York, 10006, United States', 'Train Station', 'https://maps.app.goo.gl/vxgtg49XBF42KniS8', '05:00:00', '23:00:00'),
--- (2, 3, 'New York University', 'New York, NY 10012, United States', 'University', 'https://maps.app.goo.gl/hhitytMNzU5awz3m9', '09:00:00', '18:00:00'),
--- (2, 4, 'Bank of America Financial Center', '50 Bayard St, New York, NY 10013, United States', 'Bank', 'https://maps.app.goo.gl/bi3799mW18z4xZMo8', '09:00:00', '17:00:00'),
--- (2, 5, '9/11 Memorial & Museum', '180 Greenwich St, New York, NY 10007, United States', 'Museum', 'https://maps.app.goo.gl/bqe8yoCUKxfzaZRHA', '10:00:00', '18:00:00'),
--- (2, 6, 'The Beekman, A Thompson Hotel, by Hyatt', '123 Nassau St, New York, NY 10038, United States', '5 Star Hotel', 'https://maps.app.goo.gl/ecbsWgFcbiMLDstj9', '00:00:00', '23:59:59');
+-- INSERT INTO Place_of_Interest (City_ID, Type_ID, Name, Address, Latitude, Longitude, Description, Photos, Opening_time, Ending_time)
+--     VALUES
+--     (2, 1, 'Reserve Cut', '40 Broad St 2nd Floor, New York, NY 10004, United States', 40.7061850023493, -74.01175457253665, 'Restaurant', 'https://i.imgur.com/YqVLAHX.jpg', '08:00:00', '22:00:00'),
+--     (2, 2, 'World Trade Center', '10007, 70 Vesey St, New York, 10006, United States', 40.712844368679654, -74.01188602903355, 'Train Station', 'https://i.imgur.com/kEm63qX.jpg', '05:00:00', '23:00:00'),
+--     (2, 3, 'New York University', 'New York, NY 10012, United States', 40.72960280456913, -73.99646090321473, 'University', 'https://i.imgur.com/vV3wKlq.jpg', '09:00:00', '18:00:00'),
+--     (2, 4, 'Bank of America Financial Center', '50 Bayard St, New York, NY 10013, United States', 40.715404959295654, -73.99693820331343, 'Bank', 'https://i.imgur.com/0bFaPxo.png', '09:00:00', '17:00:00'),
+--     (2, 5, '9/11 Memorial & Museum', '180 Greenwich St, New York, NY 10007, United States', 40.71173208843338, -74.01331474554571, 'Museum', 'https://i.imgur.com/e4ixMsP.jpg', '10:00:00', '18:00:00'),
+--     (2, 6, 'The Beekman, A Thompson Hotel, by Hyatt', '123 Nassau St, New York, NY 10038, United States', 40.71131902044953, -74.00692557728706, '5 Star Hotel', 'https://i.imgur.com/KNGQzZO.jpg', '00:00:00', '23:59:59');
+
 
 -- INSERT INTO Borough (City_ID, Name)
 -- VALUES
@@ -104,12 +102,28 @@
 -- (1, 'Tower Hamlets'),
 -- (1, 'Waltham Forest'),
 -- (1, 'Wandsworth'),
--- (1, 'Westminster'),
 -- (1, 'City of London');
+
+-- INSERT INTO Borough (City_ID, Place_ID, Name)
+-- VALUES
+-- (1, 1, 'Westminster'),
+-- (1, 2, 'Westminster'),
+-- (1, 3, 'Westminster'),
+-- (1, 4, 'Westminster'),
+-- (1, 5, 'Westminster'),
+-- (1, 6, 'Westminster');
+
+-- INSERT INTO Borough (City_ID, Place_ID, Name)
+-- VALUES
+-- (2, 1, 'Manhattan'),
+-- (2, 2, 'Manhattan'),
+-- (2, 3, 'Manhattan'),
+-- (2, 4, 'Manhattan'),
+-- (2, 5, 'Manhattan'),
+-- (2, 6, 'Manhattan');
 
 -- INSERT INTO Borough (City_ID, Name)
 -- VALUES
--- (2, 'Manhattan'),
 -- (2, 'Brooklyn'),
 -- (2, 'Queens'),
 -- (2, 'The Bronx'),
